38d9a6804e8b01bb50351efde30321171a36392a
juliaMasset <100589004+juliaMasset@users.noreply.github.com>
2022-09-28 13:40:32 +0200

    ajout welcome page + cartes dans BDD

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ListUsersComponent} from './pages/list-users/list-users.component';
 import {AddUserComponent} from './pages/add-user/add-user.component';
+import {WelcomePageComponent} from './pages/welcome-page/welcome-page.component';
 
 
 const routes: Routes = [
-  { path: '', component: ListUsersComponent },
+  { path: '', component: WelcomePageComponent },
   { path: 'list', component: ListUsersComponent },
   { path: 'add-user', component: AddUserComponent }];
 
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListUsersComponent } from './pages/list-users/list-users.component';
 import { AddUserComponent } from './pages/add-user/add-user.component';
+import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 import { HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomePageComponent,
     ListUsersComponent,
     AddUserComponent
   ],
