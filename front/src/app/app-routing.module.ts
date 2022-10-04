8c92d85b1649dcd95a3c087b573ccc8a6793e48f
valentine2206 <valentine.cuisinier@epfedu.fr>
2022-10-04 17:20:42 +0200

    choix équipes page

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import {ListCartesComponent} from './pages/list-cartes/list-cartes.component';
 import {AddCarteComponent} from './pages/add-carte/add-carte.component';
 import {WelcomePageComponent} from './pages/welcome-page/welcome-page.component';
 import {GameComponent} from './pages/game/game.component';
+import {TeamsPageComponent} from './pages/teams-page/teams-page.component';
 
 
 const routes: Routes = [
   { path: '', component: WelcomePageComponent },
   { path: 'add-carte', component: AddCarteComponent },
   { path: 'list-cartes', component: ListCartesComponent },
-  { path: 'game', component: GameComponent}];
+  { path: 'game', component: GameComponent},
+  {path: 'teams-page', component: TeamsPageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
