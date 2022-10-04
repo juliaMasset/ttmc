e5e9b209474ea26f2a2f0accbd96a66c0576bb41
juliaMasset <100589004+juliaMasset@users.noreply.github.com>
2022-10-04 17:37:34 +0200

    etape 1 jeu

--- front/src/app/pages/game/game.component.ts
+++ front/src/app/pages/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {CarteService} from '../../services/carte.service';
 
 @Component({
     selector: 'app-game',
@@ -7,12 +8,33 @@ import { Component } from '@angular/core';
 })
 export class GameComponent {
 
-    joueurQuiJoue = -1
-    constructor() {
+    etape = 1
+    lastEtape =4
+    nombreDeJoueur = 3
+    joueurQuiJoue = 1
+    numCaseJoueurs = [1,1,1]
+    carte = null
+    constructor(private carteService : CarteService) {
     }
 
     nextJoueur(){
         this.joueurQuiJoue ++
+        if (this.joueurQuiJoue>this.nombreDeJoueur) {
+            this.joueurQuiJoue=1
+        }
+    }
+
+    tirerCarte(){
+        this.carteService.tirerCarte(this.numCaseJoueurs[this.joueurQuiJoue-1]).subscribe(carte => this.carte = carte)
+        this.nextEtape()
+    }
+
+    nextEtape(){
+        this.etape ++
+        if (this.etape>this.lastEtape) {
+            this.nextJoueur()
+            this.etape=1
+        }
     }
 
 }
