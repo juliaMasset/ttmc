b2e0b3ff40d77db440fb14400dec2219284ce841
juliaMasset <100589004+juliaMasset@users.noreply.github.com>
2022-10-10 11:18:51 +0200

    ajout page winner

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import {AddCarteComponent} from './pages/add-carte/add-carte.component';
 import {WelcomePageComponent} from './pages/welcome-page/welcome-page.component';
 import {GameComponent} from './pages/game/game.component';
 import {TeamsPageComponent} from './pages/teams-page/teams-page.component';
-
+import {WinnerComponent} from './pages/winner/winner.component';
 
 const routes: Routes = [
   { path: '', component: WelcomePageComponent },
   { path: 'add-carte', component: AddCarteComponent },
   { path: 'list-cartes', component: ListCartesComponent },
   { path: 'game', component: GameComponent},
-  {path: 'teams-page', component: TeamsPageComponent}];
+  {path: 'teams-page', component: TeamsPageComponent},
+  {path: 'winner', component: WinnerComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
