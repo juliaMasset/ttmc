1db426e9179d588d5727ecd0e72885b3a313fe5c
juliaMasset <100589004+juliaMasset@users.noreply.github.com>
2022-10-11 16:29:13 +0200

    podium update

--- front/src/app/pages/winner/winner.component.ts
+++ front/src/app/pages/winner/winner.component.ts
@@ -10,15 +10,22 @@ import {GameModel} from '../../models/game.model';
 export class WinnerComponent {
 
     game : GameModel
-    premier = `joueur${this.gameService.game.winner}`
     scoresJoueurs = new Map()
     classement = new Array()
+    premier = null
+    fondJ1=null
+    fondJ2=null
+    fondJ3=null
 
     constructor(private gameService : GameService) {
         this.game=this.gameService.game
+        this.premier = `joueur${this.gameService.game.winner}`
         this.scoresJoueurs.set(1, this.gameService.game.score[0])
         this.scoresJoueurs.set(2, this.gameService.game.score[1])
         this.scoresJoueurs.set(3, this.gameService.game.score[3])
         this.classement = Array.from(new Map([...this.scoresJoueurs].sort((a, b)=>b[1]-a[1])).keys())
+        this.fondJ1=`fondJ${this.gameService.game.winner}`
+        this.fondJ2 = `fondJ${this.classement[1]}`
+        this.fondJ3 = `fondJ${this.classement[2]}`
     }
 }
